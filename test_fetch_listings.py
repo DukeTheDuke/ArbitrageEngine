299641fc2acc8520b04d8700f201e9632f3a00a5
DukeTheDuke <dukeakj@gmail.com>
2025-07-19 14:34:36 -0500

    Add async fetch_listings test

---
+++ test_fetch_listings.py
@@ -0,0 +1,35 @@
+import pytest
+from ArbitrageEngine import ArbitrageEngine
+
+@pytest.mark.asyncio
+async def test_fetch_listings_combines_results(monkeypatch):
+    engine = ArbitrageEngine(search_terms=[])
+
+    expected_fb = [{"title": "from facebook", "price": None, "url": None}]
+    expected_ebay = [{"title": "from ebay", "price": None, "url": None}]
+    expected_craigslist = [{"title": "from craigslist", "price": None, "url": None}]
+    expected_aliexpress = [{"title": "from aliexpress", "price": None, "url": None}]
+
+    async def fake_fb(self):
+        return expected_fb
+
+    async def fake_ebay(self):
+        return expected_ebay
+
+    async def fake_craigslist(self):
+        return expected_craigslist
+
+    async def fake_aliexpress(self):
+        return expected_aliexpress
+
+    monkeypatch.setattr(ArbitrageEngine, "query_facebook", fake_fb)
+    monkeypatch.setattr(ArbitrageEngine, "query_ebay", fake_ebay)
+    monkeypatch.setattr(ArbitrageEngine, "query_craigslist", fake_craigslist)
+    monkeypatch.setattr(ArbitrageEngine, "query_aliexpress", fake_aliexpress)
+
+    listings = await engine.fetch_listings()
+
+    for listing in (
+        expected_fb + expected_ebay + expected_craigslist + expected_aliexpress
+    ):
+        assert listing in listings
